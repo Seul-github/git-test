523ce0f70f760d9600222193a0012e9e974355ef
Seul-github <gooding0814@gmail.com>
2025-02-23 20:33:01 +0900

    design: 삭제

--- app/page.js
+++ app/page.js
@@ -4,7 +4,6 @@ export default function Home() {
     <div>
       <h4 className="title">애플후레시</h4>
       <p className="title-sub">by dev lee</p>
-      <p className="title-sub">by dev lee</p>
     </div>
   );
 }
